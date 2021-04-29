251e964b34ada46cc335304a8bf8b202a828e107
amineboubaker <amine.boubaker001@gmail.com>
2021-04-29 16:59:33 +0200

    myapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
+import NavBar from './components/navbar/navbar';
+
+import Footer from './components/footer/footer';
+import Suggestion from './components/suggestion/suggestion';
+import Comments from './components/comments/Comments';
+import {MainVideo} from './components/mainvideo/MainVideo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      
+      <MainVideo/>
+      <Suggestion />
+      <Comments />
+      <Footer />
+
+      
+      
+      
+
+      
+      
+
+
+    
     </div>
   );
 }
